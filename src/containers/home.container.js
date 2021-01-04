1e273858000b54d49dcce7cb0f9212a9f20ad5ba
szymenn <ilikedzimi@gmail.com>
2021-01-04 18:35:02 +0100

    add regions list and change grid to box

--- src/containers/home.container.js
+++ src/containers/home.container.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import NavMenu from "./nav-menu.container";
 import MapWrapper from "../components/map-wrapper.component";
-import { Box, Container, Grid, List, ListItem, makeStyles, Paper } from "@material-ui/core";
+import { Box, Container, Grid, List, ListItem, makeStyles, Paper, Card, Typography } from "@material-ui/core";
 import { Bar } from "react-chartjs-2";
 import { connect } from 'react-redux';
 import Charts from '../components/charts.component';
+import { SearchCountriesByRegionRequest } from '../redux/actions/countries';
 
 
 const mapStateToProps = (state) => ({
@@ -24,6 +25,9 @@ const useStyles = makeStyles((theme) => ({
     charts: {
         padding: theme.spacing(2),
         textAlign: 'center'
+    },
+    card: {
+        minWidth: 275,
     }
 }));
 
@@ -31,43 +35,44 @@ const Home = (props) => {
 
     const classes = useStyles();
 
-    return (
-        <div style={{width: '100%'}}>
-        <NavMenu></NavMenu>
 
-        <div className={classes.root}>
-      <Grid container spacing={0}>
-        <Grid item xs={6} >
-            <Charts countries={props.countries}></Charts>
-        </Grid>
-        <Grid item xs={4} style={{width: '80%'}}>
-        <MapWrapper 
-                 countries={props.countries}
-                 googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
-                 loadingElement={<div style={{ height: `50%` }} />}
-                 containerElement={<div style={{ height: `100vh` }} />}
-                 mapElement={<div style={{ height: `50%` }} />}
-             />
-        </Grid>
-        <Grid item xs={2}>
-            <List>
-                <ListItem>
-                    HEH
-                </ListItem>
-                <ListItem>
-                    HEH
-                </ListItem>
-                <ListItem>
-                    HEH
-                </ListItem>
-                <ListItem>
-                    HEH
-                </ListItem>
-            </List>
-        </Grid>
-      </Grid>
-    </div>
-    </div>
+
+    const handleClick = (name) => {
+        props.dispatch(SearchCountriesByRegionRequest(name));
+    }
+
+
+    return (
+        <div style={{ width: '100%' }}>
+         <NavMenu />
+
+            <Box display="flex" flexDirection="row">
+                <Box>
+                <List>
+                            <ListItem button onClick={() => handleClick('Africa')}>Africa</ListItem>
+                            <ListItem button onClick={() => handleClick('Americas')}>Americas</ListItem>
+                            <ListItem button onClick={() => handleClick('Asia')}>Asia</ListItem>
+                            <ListItem button onClick={() => handleClick('Europe')}>Europe</ListItem>
+                            <ListItem button onClick={() => handleClick('Oceania')}>Oceania</ListItem>
+                        </List> 
+                    </Box>
+                    <Box alignSelf="center" style={{ width: '100%' }}>
+                        <MapWrapper
+                            countries={props.countries}
+                            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBpJFnOEgJOdSsSSd3jzGdVqzz4EGqb5s0&v=3.exp&libraries=geometry,drawing,places"
+                            loadingElement={<div style={{ height: `100%` }} />}
+                            containerElement={<div style={{ height: `50vh` }} />}
+                            mapElement={<div style={{ height: `100%` }} />}
+                        />
+                    </Box>
+            </Box>
+        <Box flexDirection="column">
+                    <Box alignSelf="center" style={{ width: '100%' }}>
+                        <Charts countries={props.countries}></Charts>
+                    </Box>
+        </Box>
+
+        </div>
     )
 
 }
@@ -78,21 +83,37 @@ export default connect(mapStateToProps)(Home);
 
 
 
-// <div style={{ width: '100%' }}>
-//     <Box display="flex" flexDirection="column">
-//         <NavMenu />
-//         <Box alignSelf="center" style={{ width: '80%' }}>
-//             <MapWrapper
-//                 countries={props.countries}
-//                 googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBpJFnOEgJOdSsSSd3jzGdVqzz4EGqb5s0&v=3.exp&libraries=geometry,drawing,places"
-//                 loadingElement={<div style={{ height: `100%` }} />}
-//                 containerElement={<div style={{ height: `100vh` }} />}
-//                 mapElement={<div style={{ height: `100%` }} />}
-//             />
-//         </Box>
-//         <Box alignSelf="center" style={{ width: '80%' }}>
-//             <Charts></Charts>
-//         </Box>
 
-//     </Box>
-// </div>
+
+
+    // <div style={{width: '100%'}}>
+    //     <NavMenu></NavMenu>
+
+    //     <div className={classes.root}>
+    //   <Grid container spacing={0}>
+    //   <Grid item xs={4}>
+    //         <List>
+    //             <ListItem button  onClick={() => handleClick('Africa')}>Africa</ListItem>
+    //             <ListItem button onClick={() => handleClick('Americas')}>Americas</ListItem>
+    //             <ListItem button onClick={() => handleClick('Asia')}>Asia</ListItem>
+    //             <ListItem button onClick={() => handleClick('Europe')}>Europe</ListItem>
+    //             <ListItem button onClick={() => handleClick('Oceania')}>Oceania</ListItem>
+    //         </List>
+    //     </Grid>
+
+    //     <Grid item xs={8} >
+    //     <MapWrapper 
+    //              countries={props.countries}
+    //              googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
+    //              loadingElement={<div style={{ height: `50%` }} />}
+    //              containerElement={<div style={{ height: `100vh` }} />}
+    //              mapElement={<div style={{ height: `50%` }} />}
+    //          />
+    //     </Grid>
+
+    //     <Grid item xs={12} >
+    //         <Charts countries={props.countries}></Charts>
+    //     </Grid>
+    //   </Grid>
+    // </div>
+    // </div>
