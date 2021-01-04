b8feba9af3e64c9e5ac4cfcbc59856535161f021
szymenn <ilikedzimi@gmail.com>
2021-01-04 18:33:26 +0100

    add name label to country markers

--- src/components/map-wrapper.component.js
+++ src/components/map-wrapper.component.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { GoogleMap, Marker, withGoogleMap, withScriptjs } from "react-google-maps";
 import { Container, Grid } from "@material-ui/core";
+import { MarkerWithLabel } from 'react-google-maps/lib/components/addons/MarkerWithLabel';
 
 
 const MapWrapper = withScriptjs(withGoogleMap((props) => {
+    const google = window.google
+
     const markers = props.countries.map((country) => {
-        return <Marker key={country.name} position={{ lat: country.latlng[0], lng: country.latlng[1] }} />
+        if (country.latlng.length !== 0) {
+            return (
+
+                <div key={country.name}>
+                    <MarkerWithLabel
+                        position={{ lat: country.latlng[0], lng: country.latlng[1] }}
+                        labelAnchor={new google.maps.Point(0, 0)}
+                        labelStyle={{ fontSize: "10px", backgroundColor: 'none', color: 'red' }}
+                    >
+                        <div>{country.name}</div>
+                    </MarkerWithLabel>
+                </div>
+            )
+        }
     });
 
     return (
@@ -22,3 +38,7 @@ const MapWrapper = withScriptjs(withGoogleMap((props) => {
 
 export default MapWrapper;
 
+    //     return <MarkerWithLabel key={country.name} position={{ lat: country.latlng[0], lng: country.latlng[1] }} 
+    //   labelStyle={{backgroundColor: "yellow", fontSize: "32px", padding: "16px"}}>
+    //         HEHEHE
+    //     </MarkerWithLabel>
